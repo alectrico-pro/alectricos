c18c4352692dda7b173efe808abf0d7e8a3a387b
alectrico <ventas@alectrico.cl>
2023-04-10 05:11:52 -0400

    Poniendo contraseña al controlador

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-#	http_basic_authenticate_with name: "gope", password: "crisis"
+	http_basic_authenticate_with name: "gope", password: "crisis"
 end
